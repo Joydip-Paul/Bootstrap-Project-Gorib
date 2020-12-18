7c91c43ea35ce2b061ae1f09a680b7bbc3aa3055
Joydip Paul <joydippaul72@gmail.com>
2020-12-19 02:47:54 +0600

    two section added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,50 @@
-$('.banner-area').owlCarousel({
-    loop:true,
-    dots:true,
-    nav:false,
-    autoplay:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:1
+// $('.banner-area').owlCarousel({
+//     loop:true,
+//     dots:true,
+//     nav:false,
+//     autoplay:true,
+//     responsive:{
+//         0:{
+//             items:1
+//         },
+//         600:{
+//             items:3
+//         },
+//         1000:{
+//             items:1
+//         }
+//     }
+// })
+// $(document).ready(function() {
+//     $('.project-popup').magnificPopup({type:'image'});
+//   });
+
+  $(document).ready(function(){
+    $('.banner-area').owlCarousel({
+        loop:true,
+        dots:true,
+        nav:false,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:1
+            }
         }
-    }
-})
+    });
+    $('.project-popup').magnificPopup({
+        type:'image',
+        gallery: {
+            enabled: true
+          },
+    });
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+  });
