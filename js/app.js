958ae2548af49cff5af11d52b6e05956fc03837f
Joydip Paul <joydippaul72@gmail.com>
2020-12-19 01:03:42 +0600

    50% done

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$('.banner-area').owlCarousel({
+    loop:true,
+    dots:true,
+    nav:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
